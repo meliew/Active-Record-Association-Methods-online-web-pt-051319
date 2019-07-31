94c87d893cdda1a2a845aceb035a56e263c9867b
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-31 03:05:48 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
 
-    Song.find_or_create_by(artists: "Drake")
+    Song.find_or_create_by(artist: "Drake")
     song.artist("Drake")
 
     #find or create
