ca6d95e93fb53e839b146f15aa8ff56986759e07
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-31 02:54:47 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,14 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def get_genre_of_first_song
+  end
+
+  def song_count
+  end
+
+  def genre_count
+    self.genres.length
+  end
 end
